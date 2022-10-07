0c72a69c79cd12827a306540fe4c1647aa5315b9
MrFlexEc <itsthebryanyt@gmail.com>
2022-10-07 16:59:51 -0500

    Codificacion del metodo Async Await

--- primerParcial/practicaBusqueda/asyncFind.js
+++ primerParcial/practicaBusqueda/asyncFind.js
@@ -1,1 +1,48 @@
+//Importacion de los datos del archivo de datos
+const {autos, mantenimientos} = require('./datos');
 //Async busqueda de datos
+async function buscarAutoPorId(id){
+    //Creacion del objeto de clase auto para usar el metodo FIND
+    const auto = autos.find((auto)=> auto.id===id);
+    if (!auto) {
+        //Creacion de error para controlar excepcion
+        const error = new Error();
+        error.message="Lo sentimos. No encontramos el auto";
+        throw error;
+    }
+    //Retorno del objeto
+    return auto;
+}
+
+async function buscarMantenimientoPorId(id) {
+    //Creacion del objeto de clase MANTENIMIENTO para usar el metodo FIND
+    const mantenimiento = mantenimientos.find((mantenimiento)=> mantenimiento.id===id);
+    if (!mantenimiento) {
+        //Creacion de error para controlar excepcion
+        const error = new Error();
+        error.message="Lo sentimos. No tenemos registro de este mantenimiento";
+        throw error;
+    }
+    //Retorno del objeto
+    return mantenimiento;
+}
+
+(async ()=>{
+    try
+    {
+        //Creacion de objetos AUTO y MANTENIMIENTO con propiedad await para usar la funcion async
+        const auto = await buscarAutoPorId(2);
+        const mantenimiento = await buscarMantenimientoPorId(1);
+        //Agregacion del objeto AUTO al objeto MANTENIMIENTO
+        mantenimiento.idAuto=auto;
+        //IMPRESION DE DATOS
+        console.log("ID: " + mantenimiento.id + " \nAUTO: ", mantenimiento.idAuto, " | FECHA: " + mantenimiento.fechaMantenimiento + 
+        " | DETALLE: " + mantenimiento.detalle);
+    }
+    catch (err)
+    {
+        //Excepcion controlada
+        console.log(err.message)
+    }
+}
+)();
--- primerParcial/practicaBusqueda/datos.js
+++ primerParcial/practicaBusqueda/datos.js
@@ -1,9 +1,44 @@
-//Arreglos con 5 objetos
+//Arreglos con 5 objetos//
 
 //Arreglos de los datos de autos
-const autos= [];
+const autos= [
+    {
+        id:1, descripcion:'Usado', placa:'MBF-1907', Color:'Negro'
+    },
+    {
+        id:2, descripcion:'Nuevo', placa:'OBF-0897', Color:'Blanco'
+    },
+    {
+        id:3, descripcion:'Usado', placa:'MBA-107', Color:'Verde'
+    },
+    {
+        id:4, descripcion:'Semi-nuevo', placa:'MBC-0097', Color:'Rojo'
+    },
+    {
+        id:5, descripcion:'Usado', placa:'GOU-098', Color:'Azul'
+    },
+    {
+        id:6, descripcion:'Nuevo', placa:'MBG-0965', Color:'Negro'
+    }
+];
 //Arreglos de datos de mantenimiento
-const mantenimiento=[];
+const mantenimientos=[
+    {
+        id:1, idAuto:2, fechaMantenimiento:"03-feb-2022", detalle:"Costo de $10"
+    },{
+        id:2, idAuto:1, fechaMantenimiento:"10-07-2022", detalle:"Costo de $15"
+    },{
+        id:3, idAuto:3, fechaMantenimiento:"05-06-2022", detalle:"Costo de $35"
+    },{
+        id:4, idAuto:1, fechaMantenimiento:"30-12-2021", detalle:"Costo de $50"
+    },{
+        id:5, idAuto:4, fechaMantenimiento:"10-11-2021", detalle:"Costo de $10"
+    },{
+        id:6, idAuto:6, fechaMantenimiento:"07-10-2022", detalle:"Costo de $30"
+    },{
+        id:7, idAuto:5, fechaMantenimiento:"18-04-2022", detalle:"Costo de $10"
+    }
+];
 
 //Exportación de los arreglos
-module.exports= {autos,mantenimiento};
+module.exports= {autos,mantenimientos};
